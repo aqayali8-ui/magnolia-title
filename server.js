8094ce769dd478118b7471c555659f0a46c6cd50
aqayali8-ui <aqayali8@gmail.com>
2026-05-31 12:52:01 +0000

    Launch Magnolia Title Services website

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Magnolia Title running on port ${PORT}`));
